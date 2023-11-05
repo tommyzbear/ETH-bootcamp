6e6183a057f4171d12d4ef423e85e5e1e1d51b26
tommyzbear <ajdsenwill@hotmail.com>
2023-11-05 11:20:59 +0000

    Update weekly project evidence

--- Week2/README.md
+++ Week2/README.md
@@ -22,3 +22,28 @@
 ### Transaction Hash2: https://sepolia.etherscan.io/tx/0x65662b7bb5dc60444782554a851682dc127668226f0789481d56a9cdfadf8c22
 
 ![Screenshot](screenshots/give_voting_right.png)
+
+## Reverted Delegation
+
+Delegation to self is reverted with reason `Self-delegation is disallowed.`
+![Screenshot](screenshots/reverted_delegation.png)
+
+## Delegate Voting Right
+
+Delegate voting right to another address
+
+### Transaction Hash1: https://sepolia.etherscan.io/tx/0xda3e9c6e6a52a6ef68bb376f2ea49cc12e492977bb934fab773a5a653dad1bb9
+
+## Vote for Proposal
+
+Vote for a proposal
+
+### Transaction Hash: https://sepolia.etherscan.io/tx/0x15a4ab2a2dc7675882447e55c38a85f5a0832a40e62fed72108ae3b89665d45e
+
+![Screenshot](screenshots/vote_for_proposal.png)
+
+## Check votes on etherscan
+
+Proposal should receive 2 votes, since the wallet that initiate the transaction has 1 delegation
+
+![Screenshot](screenshots/vote_check.png)
--- Week2/scripts/InteractWithBallotContract.ts
+++ Week2/scripts/InteractWithBallotContract.ts
@@ -4,7 +4,8 @@ import { Ballot, Ballot__factory } from "../typechain-types";
 dotenv.config();
 
 async function main() {
-  const provider = ethers.getDefaultProvider("sepolia");
+  // const provider = ethers.getDefaultProvider("sepolia");
+  const provider = new ethers.AlchemyProvider("sepolia", process.env.ALCHEMY_API_KEY);
   const lastBlock = await provider.getBlock("latest");
   console.log(`Last block number: ${lastBlock?.number}`);
   const lastBlockTimestamp = lastBlock?.timestamp ?? 0;
@@ -23,25 +24,34 @@ async function main() {
   const parameters = process.argv.slice(2);
   if (!parameters || parameters.length < 2) throw new Error("Parameters not provided");
   const contractAddress = parameters[0];
-  const address1 = parameters[1];
-  //   const address2 = parameters[2];
+  // const address1 = parameters[1];
+  // const address2 = parameters[2];
+  const proposalToVote = parameters[1];
 
   const ballotFactory = new Ballot__factory(wallet);
   const ballotContract = (await ballotFactory.attach(contractAddress)) as Ballot;
 
+  // Read proposals
   //   for (let index = 0; index < 3; index++) {
   //     const proposal = await ballotContract.proposals(index);
   //     const name = await ethers.decodeBytes32String(proposal.name);
   //     console.log({ index, name, proposal });
   //   }
 
-  const tx1 = await ballotContract.giveRightToVote(address1);
-  const receipt1 = await tx1.wait();
-  console.log(`Give right to vote for address1 transaction completed ${receipt1?.hash}`);
+  // Give right to vote
+  // const tx1 = await ballotContract.giveRightToVote(address1);
+  // const receipt1 = await tx1.wait();
+  // console.log(`Give right to vote for address1 transaction completed ${receipt1?.hash}`);
 
-  //   const tx2 = await ballotContract.giveRightToVote(address2);
-  //   const receipt2 = await tx2.wait();
-  //   console.log(`Give right to vote for address2 transaction completed ${receipt2?.hash}`);
+  // Delegate vote to other user
+  // const tx = await ballotContract.delegate(address1);
+  // const receipt = await tx.wait(5);
+  // console.log(`Delegate voting right to address ${address1}, tx: ${receipt?.hash}`);
+
+  // Vote for proposal
+  const tx = await ballotContract.vote(proposalToVote);
+  const receipt = await tx.wait(2);
+  console.log(`Vote for proposal, tx: ${receipt?.hash}`);
 }
 
 main().catch(error => {
